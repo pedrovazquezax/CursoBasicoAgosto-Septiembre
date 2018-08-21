1b556f5ba1ea9b282c7de3aefa86508ad39e91f7
Pedro Vazquez <pedrovazquezax@gmail.com>
2018-08-21 17:13:41 -0500

    lo visto en la primer clase

---
+++ Clase1.playground/Contents.swift
@@ -0,0 +1,87 @@
+//: Playground - noun: a place where people can play
+
+import Foundation
+//Tipo de delcaracion   nombre variable        Valor
+  var                     str                = "Hello, playground"
+
+
+//Variabe , tipado inferido
+var nombre = "Pedro"
+var primerApellidoDeNacimiento : String = "Vazquez"
+
+//Constante
+let pi : Int = 78
+
+nombre = "Antonio"
+
+var x = 0.0, y = 0.0 , z = 0.0
+var rojo,verde,azul:Int
+
+var 😎 = "😝"
+
+//Tipos de datos
+
+var entero : Int = 23
+var flotante : Float = 1244.3
+var doble : Double = 56.5787
+var booleano : Bool = true
+var cadena : String = "hola"
+var caracter : Character = "t"
+var cualquiera : Any  = "cambiame por otro tipo de dato"
+
+// Operadores matematicos
+
+var a =  10
+a = a + 1
+a = a - 1
+a = a * a
+a = a / a
+a = 9 % 4
+
+var b = 10
+
+// incremento
+
+b += 10
+
+//decremento
+b -= 10
+
+//operadores Logicos
+var  c = 1.1
+var d = 2.2
+
+//mayor que
+c > 3
+//menor que
+c < 4
+
+//mayor igual que
+c >= 3
+//menor igual que
+c <= 1.1
+
+// igual que
+nombre == "pedro"
+//diferente
+nombre != "Antonio"
+//negado NOT
+var vivoEnElSemestre = true
+!vivoEnElSemestre
+
+//And
+true && true
+true && false
+//Or
+true || false
+true || true
+false || false
+
+
+
+
+
+
+
+
+ 
---
+++ miPrimerApp/miPrimerApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  miPrimerApp
+//
+//  Created by Pedro Antonio Vazquez Rodriguez on 21/08/18.
+//  Copyright © 2018 Pedro Antonio Vazquez Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
