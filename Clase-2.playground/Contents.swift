d22f79a20c068bd32ed62e4bea220ec560b5bc9e
Pedro Antonio Vazquez Rodriguez <pedroantoniovazquezrodriguez@MacBook-Pro-2.local>
2018-08-23 15:02:16 -0500

    segunda clase

---
+++ Clase-2.playground/Contents.swift
@@ -0,0 +1,81 @@
+//////Segunda clase/////
+
+
+let favoriteBook = "El padrino"
+
+let englishGreeting = "Hello, World!"
+
+let chineseGreeting = "你好，世界!"
+
+let spanishGreeting = "¡Hola Mundo!"
+
+let russianGreeting = "Привет мир!"
+
+let japaneseGreeting = "こんにちは世界!"
+
+let 英语问候 = "Hello, World!"
+
+let 🍓🍏🍒🍐🍋🍇 = "Fruit Salad"
+
+
+let peliculaFavorita = "Interestelar"
+
+
+let userName = "Valen"
+let likeYourPost = "Like"
+let message = userName + " " + likeYourPost
+
+
+let fullName = "Valen" + " Montoya"
+
+let city = "Medellín"
+
+let welcomeString = "Hello " + fullName + ", Welcome to " + city + "!"
+
+//Interpolación
+
+let firstName = "Julio"
+let cdmx = "Ciudad de México"
+let Bienvenido = "Hello \(firstName), welcome to \(cdmx)"
+
+
+//Funciones
+func primerFuncion(){
+    print("Hola")
+    
+}
+
+primerFuncion()
+
+func suma(n1:Int,n2:Int){
+    
+    print(n1+n2) // Utiliza menos memoria
+    
+    let resultado = n1 + n2 // Utiliza más memoria por el hecho de crear otra variable
+print(resultado)
+    
+}
+
+suma(n1: 3, n2: 2)
+suma(n1: 1, n2: 5)
+
+//Canción
+
+//Función canción
+
+func canción(){
+let meReuso = "Bby no"
+let meReuso2 = "Me Reuso a darte un último beso"
+let meReuso3 = "asi que"
+let meReuso4 = "guardalo"
+let meReuso5 = "Para que la próxima vez te lo de"
+let meReuso6 = "haciendolo"
+
+let canción1 = "\(meReuso), \(meReuso), \(meReuso2) \(meReuso3) \(meReuso4), \(meReuso4). \(meReuso5) \(meReuso6), \(meReuso6)."
+    print(canción1)
+}
+
+canción()
+
+
+
